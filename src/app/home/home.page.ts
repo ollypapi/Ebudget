57f489efc0855bff21d40419092bf446f9793367
ollypapi <ollypapi1@gmail.com>
2020-07-15 01:38:25 +0100

    initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {AlertController} from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(public alertCtrl: AlertController) {}
+  expenses: any = [];
+  total = 0;
+   budget = {
+    expense: '',
+    amount: 0
+  };
+  add() {
+    if (this.budget.expense === '' || this.budget.amount === 0) {
+        this.showAlert();
+        return;
+    }
+    this.expenses.push(this.budget.expense + ' :' + ' 4#' + this.budget.amount);
+    this.total += this.budget.amount;
+    console.log(this.expenses);
+    this.budget = {expense: ' ', amount: 0};
+  }
 
-  constructor() {}
-
+  delete() {
+    this.budget = {expense: ' ', amount: 0};
+  }
+  async showAlert() {
+    const alert = await this.alertCtrl.create({
+      header: 'Invalid Details',
+      message: 'Kindly check what you input and make correction',
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
 }
